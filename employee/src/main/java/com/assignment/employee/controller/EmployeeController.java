338115d5a09730e769d63413cafbf348bb6e4f91
dassusobhan <63341093+dassusobhan@users.noreply.github.com>
2020-04-10 17:19:31 +0530

    Update EmployeeController.java
    Changed in controller class

--- employee/src/main/java/com/assignment/employee/controller/EmployeeController.java
+++ employee/src/main/java/com/assignment/employee/controller/EmployeeController.java
@@ -4,6 +4,7 @@ import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,19 +21,23 @@ public class EmployeeController {
 	private EmployeeService service;
 	
 	@GetMapping("/employees/dept/{deptNo}")
-	public List<EmployeeModel> get_all_employees_name_birth_date_by_dept_no(@PathVariable("deptNo") String deptNo){
-		return service.get_all_employees_name_birth_date_by_dept_no(deptNo);
+	public ResponseEntity<List<EmployeeModel>> get_all_employees_name_birth_date_by_dept_no(@PathVariable("deptNo") String deptNo){
+		List<EmployeeModel> employees= service.get_all_employees_name_birth_date_by_dept_no(deptNo);
+		return ResponseEntity.ok(employees);
 		
 	}
 	
 	@GetMapping("/employees/hiredate/{hiredate}/salary/{salary}")
-	public List<Employee> get_employees_hired_after_date_and_min_salary(@PathVariable("salary") Integer vSalary,@PathVariable("hiredate") String hire_date) {
-		return service.get_employees_hired_after_date_and_min_salary(vSalary, hire_date);
+	public ResponseEntity<List<Employee>> get_employees_hired_after_date_and_min_salary(@PathVariable("salary") Integer vSalary,@PathVariable("hiredate") String hire_date) {
+		List<Employee> employees=service.get_employees_hired_after_date_and_min_salary(vSalary, hire_date);
+		return ResponseEntity.ok(employees);
 	}
 	
     @DeleteMapping("employees/salaries/employee/hiredate/{hiredate}")
-	public void delete_employee_of_salaries_hire_date_before_given_date(@PathVariable("hiredate") String hireDate) {
+	public ResponseEntity<?> delete_employee_of_salaries_hire_date_before_given_date(@PathVariable("hiredate") String hireDate) {
 		service.delete_employee_of_salaries_hire_date_before_given_date(hireDate);
+		
+		return ResponseEntity.noContent().build();
 	}
 	
 }
